98172ae136596c00185e7568f5e53e8ee9fe9e17
Satvik435 <86778274+Satvik435@users.noreply.github.com>
2021-07-12 16:28:12 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,20 @@
+var marks = [35,38,42,45,43,34,46,41,48,32];
+
+
+function setup() 
+{
+  createCanvas(400,400);
+for(var a = 0;a<marks.length;a++ ){ 
+  if(marks[a]>=45){
+    console.log (marks[a])
+  }
+}
+}
+
+function draw() 
+{
+  background(30);
+}
+
+ 
+
